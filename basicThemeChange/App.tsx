9bba57b22f28588d15899a5d4d2b90b6e14fa330
Sahil Tiwari <sahiltiwari.contactme@gmail.com>
2025-11-04 13:37:25 +0530

    Basic theme changing practice

--- basicThemeChange/App.tsx
+++ basicThemeChange/App.tsx
@@ -1,45 +1,39 @@
-/**
- * Sample React Native App
- * https://github.com/facebook/react-native
- *
- * @format
- */
-
-import { NewAppScreen } from '@react-native/new-app-screen';
-import { StatusBar, StyleSheet, useColorScheme, View } from 'react-native';
-import {
-  SafeAreaProvider,
-  useSafeAreaInsets,
-} from 'react-native-safe-area-context';
-
-function App() {
-  const isDarkMode = useColorScheme() === 'dark';
-
+import { View, Text, useColorScheme, StyleSheet } from 'react-native';
+import React from 'react';
+export default function App() {
+  const theme = useColorScheme() === 'dark';
   return (
-    <SafeAreaProvider>
-      <StatusBar barStyle={isDarkMode ? 'light-content' : 'dark-content'} />
-      <AppContent />
-    </SafeAreaProvider>
-  );
-}
-
-function AppContent() {
-  const safeAreaInsets = useSafeAreaInsets();
-
-  return (
-    <View style={styles.container}>
-      <NewAppScreen
-        templateFileName="App.tsx"
-        safeAreaInsets={safeAreaInsets}
-      />
+    <View
+      style={[styles.container, theme ? styles.darkMode : styles.lightMode]}
+    >
+      {' '}
+      <Text
+        style={[
+          styles.text,
+          theme ? styles.darkModeText : styles.lightModeText,
+        ]}
+      >
+        {' '}
+        App{' '}
+      </Text>{' '}
     </View>
   );
 }
-
 const styles = StyleSheet.create({
-  container: {
-    flex: 1,
+  container: { flex: 1, justifyContent: 'center', alignItems: 'center' },
+  text: {
+    fontSize: 24,
+  },
+  darkMode: {
+    backgroundColor: '#ffffff',
+  },
+  lightMode: {
+    backgroundColor: '#212121',
+  },
+  darkModeText: {
+    color: '#212121',
+  },
+  lightModeText: {
+    color: '#ffffff',
   },
 });
-
-export default App;
